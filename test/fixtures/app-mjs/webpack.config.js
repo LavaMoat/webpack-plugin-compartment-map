665a89e046faa3c6843974f7929e89fcd690fa42
legobt <6wbvkn0j@anonaddy.me>
2023-02-27 10:16:06 +0000

    wip: attempt bundling from processed source
    modules lack runtime at this point, so this won't work

--- test/fixtures/app-mjs/webpack.config.js
+++ test/fixtures/app-mjs/webpack.config.js
@@ -3,6 +3,40 @@ import CompartmentMapPlugin from '../../../src/plugin.mjs'
 export default {
   entry: './entry.js',
   mode: 'production',
+  externalsType: 'node-commonjs',
+  target: 'node12',
+  output: {
+    asyncChunks: false,
+    chunkFormat: 'module',
+    enabledChunkLoadingTypes: ['jsonp'],
+    clean: {
+      //keep: /\/xx\//
+    }
+  },
+  optimization: {
+    //moduleIds: false,
+    //moduleIds: 'named',
+    runtimeChunk: true,
+  },
+  module: {
+    parser: {
+      javascript: {
+        dynamicImportMode: 'eager'
+      }
+    },
+//    exprContextCritical: true,
+//    exprContextRecursive: true,
+//    exprContextRegExp: true,
+//    exprContextRequest: '.',
+//    unknownContextCritical: true,
+//    unknownContextRecursive: true,
+//    unknownContextRegExp: true,
+//    unknownContextRequest: '.',
+//    wrappedContextCritical: true,
+//    wrappedContextRecursive: true,
+//    wrappedContextRegExp: /.*/,
+//    strictExportPresence: true,
+  },
   plugins: [
     new CompartmentMapPlugin()
   ]
